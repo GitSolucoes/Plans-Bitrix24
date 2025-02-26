7befee31c19060519b514a7f6a521eff8b4eaf72
Grupo soluções dev <167912902+GitSolucoes@users.noreply.github.com>
2025-02-26 11:09:30 -0300

    Add CORS

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 from dotenv import load_dotenv
 import os
 import time
 
 app = Flask(__name__)
+CORS(app)
 
 load_dotenv()
 
